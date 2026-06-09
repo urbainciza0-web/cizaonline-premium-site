2ddd78a2ec6aead4c1fa2a568febd539f3fb29d0
urbainciza0-web <urbainciza0@gmail.com>
2026-06-09 17:20:54 +0200

    Remove middleware redirect loop

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
 import { NextResponse } from "next/server";
 
-const canonicalHost = "cizaonline.com";
 const previewNoIndexHeader = "noindex, nofollow, noarchive, nosnippet";
 
 function isVercelPreviewHost(hostname) {
@@ -8,15 +7,7 @@ function isVercelPreviewHost(hostname) {
 }
 
 export function middleware(request) {
-  const url = request.nextUrl.clone();
   const hostname = request.headers.get("host")?.split(":")[0]?.toLowerCase() || "";
-
-  if (hostname === `www.${canonicalHost}`) {
-    url.hostname = canonicalHost;
-    url.protocol = "https";
-    return NextResponse.redirect(url, 308);
-  }
-
   const response = NextResponse.next();
 
   if (isVercelPreviewHost(hostname)) {
